87b92a58081449e2dd6fe6fd62512bd98fff055d
Fabrice Gallet <74436347+fbgallet@users.noreply.github.com>
2024-03-12 23:35:14 +0100

    post

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 
 const corsOptions = {
-  origin: "https://roamresearch.com",
+  origin: "roamresearch.com",
   // optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
@@ -35,35 +35,31 @@ app.post("/", (req, res) => {
   }
 });
 
-app.post(
-  "/message",
-  /*cors({ corsOptions }),*/ async (req, res) => {
-    try {
-      res.header("Access-Control-Allow-Origin", "roamresearch.com");
-      res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-      );
-      const { key, content } = req.body;
-      if (!key || !content) {
-        res
-          .status(400)
-          .json({ message: "Valid API key & content are needed." });
-        return;
-      }
-      const anthropic = anthropicAPI(key);
-      const message = await anthropic.messages.create({
-        max_tokens: 1024,
-        messages: [{ role: "user", content }],
-        model: "claude-3-opus-20240229",
-      });
-      console.log(message.content);
-      res.status(200).json({ response: message.content[0].text });
-    } catch (error) {
-      res.status(500).json({ message: error.response });
+app.post("/message", cors({ corsOptions }), async (req, res) => {
+  try {
+    // res.header("Access-Control-Allow-Origin", "roamresearch.com");
+    // res.header(
+    //   "Access-Control-Allow-Headers",
+    //   "Origin, X-Requested-With, Content-Type, Accept"
+    // );
+    console.log("post");
+    const { key, content } = req.body;
+    if (!key || !content) {
+      res.status(400).json({ message: "Valid API key & content are needed." });
+      return;
     }
+    const anthropic = anthropicAPI(key);
+    const message = await anthropic.messages.create({
+      max_tokens: 1024,
+      messages: [{ role: "user", content }],
+      model: "claude-3-opus-20240229",
+    });
+    console.log(message.content);
+    res.status(200).json({ response: message.content[0].text });
+  } catch (error) {
+    res.status(500).json({ message: error.response });
   }
-);
+});
 
 // All other routes
 app.all("*", (req, res) => {
