19843062ade58ce86c3dffd43e907487333b6138
Fabrice Gallet <74436347+fbgallet@users.noreply.github.com>
2024-03-12 23:24:30 +0100

    res.header

--- index.js
+++ index.js
@@ -5,13 +5,9 @@ const express = require("express");
 const Anthropic = require("@anthropic-ai/sdk");
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+app.use(cors());
 
-var corsOptions = {
+const corsOptions = {
   origin: "https://roamresearch.com",
   // optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
@@ -39,26 +35,35 @@ app.post("/", (req, res) => {
   }
 });
 
-app.post("/message", cors({ corsOptions }), async (req, res) => {
-  try {
-    res.header("Access-Control-Allow-Origin", "*");
-    const { key, content } = req.body;
-    if (!key || !content) {
-      res.status(400).json({ message: "Valid API key & content are needed." });
-      return;
+app.post(
+  "/message",
+  /*cors({ corsOptions }),*/ async (req, res) => {
+    try {
+      res.header("Access-Control-Allow-Origin", "roamresearch.com");
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+      );
+      const { key, content } = req.body;
+      if (!key || !content) {
+        res
+          .status(400)
+          .json({ message: "Valid API key & content are needed." });
+        return;
+      }
+      const anthropic = anthropicAPI(key);
+      const message = await anthropic.messages.create({
+        max_tokens: 1024,
+        messages: [{ role: "user", content }],
+        model: "claude-3-opus-20240229",
+      });
+      console.log(message.content);
+      res.status(200).json({ response: message.content[0].text });
+    } catch (error) {
+      res.status(500).json({ message: error.response });
     }
-    const anthropic = anthropicAPI(key);
-    const message = await anthropic.messages.create({
-      max_tokens: 1024,
-      messages: [{ role: "user", content }],
-      model: "claude-3-opus-20240229",
-    });
-    console.log(message.content);
-    res.status(200).json({ response: message.content[0].text });
-  } catch (error) {
-    res.status(500).json({ message: error.response });
   }
-});
+);
 
 // All other routes
 app.all("*", (req, res) => {
