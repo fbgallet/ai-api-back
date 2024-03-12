ec88cdea9d8a6295ac64b652e42437afc466de05
Fabrice Gallet <74436347+fbgallet@users.noreply.github.com>
2024-03-12 13:08:25 +0100

    ready for deploiement

--- index.js
+++ index.js
@@ -55,6 +55,6 @@ app.all("*", (req, res) => {
   res.status(404).json({ message: "This route does not exist ⛔" });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server started ✅");
 });
