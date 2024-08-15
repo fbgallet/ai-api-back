e385a05d6ae2d2fe0344950cff1dd0ba67487440
Fabrice Gallet <74436347+fbgallet@users.noreply.github.com>
2024-08-15 12:13:12 +0200

    Log nb of request and tokens nb

--- routes/anthropic.js
+++ routes/anthropic.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const router = express.Router();
 router.use(express.json());
 router.use(cors()); // { origin: "roamresearch.com" }
+const requestNb = 0;
 
 router.post("/anthropic/initialize", (req, res) => {
   try {
@@ -18,6 +19,7 @@ router.post("/anthropic/initialize", (req, res) => {
 });
 
 router.post("/anthropic/message", async (req, res) => {
+  requestNb++;
   try {
     const { key, prompt, context, model, temperature } = req.body;
     if (!key && !process.env.ANTHROPIC_API_KEY) {
@@ -44,6 +46,9 @@ router.post("/anthropic/message", async (req, res) => {
     // Claude 3 Sonnet	: claude-3-sonnet-20240229
     // Claude 3.5 Sonnet	: claude-3-5-sonnet-20240620
     // Claude 3 Haiku :	claude-3-haiku-20240307
+    console.log(
+      `Request n°${requestNb}\nModel: ${message.model}\nTokens: in=${message.usage.input_tokens} out=${message.usage.input_tokens}`
+    );
     res.status(200).json({ response: message });
   } catch (error) {
     console.log(error.message);
